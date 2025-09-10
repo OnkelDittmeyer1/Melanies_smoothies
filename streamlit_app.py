4d64a4937972cbca3c0348a691f74a93092e4e0d
OnkelDittmeyer1 <sebastianruff@web.de>
2025-09-10 13:54:41 +0000

    adding fruit api call and make nutrition visible on the form

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: {st.__version__}")
@@ -20,19 +22,24 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
+
+
+
 if ingredients_list:    
  
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
         ingredients_string +=fruit_chosen + ' '
-
-   # st.write(ingredients_string)
+        st.subheader(fruit_chosen + ' Nutrition score')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        st_df= st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_smoothie + """')"""
 
-    st.write(my_insert_stmt)
+   # st.write(my_insert_stmt)
    # st.stop 
     time_to_insert = st.button ("Submit Order")
     if time_to_insert:
